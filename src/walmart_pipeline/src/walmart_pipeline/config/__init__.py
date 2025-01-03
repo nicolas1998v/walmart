2b8287fc7ef027f459e987f1164b104c64c55fd8
Nicolas Velez <57761032+nicolas1998v@users.noreply.github.com>
2025-01-03 14:40:04 +0000

    Delete src/walmart_pipeline directory

--- src/walmart_pipeline/src/walmart_pipeline/config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Configuration module providing pipeline settings through PipelineConfig class.
-"""
-
-
-from .pipeline_config import PipelineConfig
-
-# Export PipelineConfig as the public interface of this package
-__all__ = ['PipelineConfig']
